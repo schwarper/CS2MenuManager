e798165f375964581b6d89cf262bf12243cb2abd
schwarper <75811921+schwarper@users.noreply.github.com>
2025-08-04 04:23:05 +0300

    Update ProjectInfo.cs

--- CS2MenuManager/ProjectInfo.cs
+++ CS2MenuManager/ProjectInfo.cs
@@ -8,7 +8,7 @@ public static class ProjectInfo
     /// <summary>
     /// Gets the current version of the CS2MenuManager.
     /// </summary>
-    public const string Version = "v38";
+    public const string Version = "v39";
 
     /// <summary>
     /// Gets the author of the CS2MenuManager.
